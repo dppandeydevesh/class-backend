59ba0fd4f75cbb9fd085a00ed1a4a9947417dd5b
Devesh Pandey <pandeydevesh33@gmail.com>
2026-03-31 15:47:56 +0530

    Added remaining files

--- class-backend/server/app.js
+++ class-backend/server/app.js
@@ -4,9 +4,32 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+const homePageContent = {
+    header: { title: "GLA Header" },
+    content: { title: "GLA Content" },
+    footer: { title: "GLA Footer" },
+    data: {
+        universityName: "GLA University",
+        students: [
+            { name: "Student 1", dept: "CSE" },
+            { name: "Student 2", dept: "CSE" },
+        ],
+},
+};
+
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', homePageContent.data);
 });
+app.get('/header', (req, res) => {
+    res.render('header', homePageContent.header);
+});
+app.get('/content', (req, res) => {
+    res.render('content', homePageContent.content);
+});
+app.get('/footer', (req, res) => {
+    res.render('footer', homePageContent.footer);
+});
+
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const authRoutes = require('./routes/authRoutes');
